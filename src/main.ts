e4ba4932ebdd64ed5dc89e4223b667373029c065
PC <komarovitstudies@gmail.com>
2025-06-04 14:43:35 +0300

    #7: Changed port

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Global prefix for all routes - SET THIS FIRST
   app.setGlobalPrefix('api');
 
@@ -34,7 +35,7 @@ async function bootstrap() {
       origin: 'http://localhost:5173',
       credentials: true,
   });
-  
+
   // Enable global validation and transformation pipes
   app.useGlobalPipes(
     new ValidationPipe({
@@ -44,7 +45,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = configService.get<number>('PORT') || 3000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
